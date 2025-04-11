741600333d811f18c35abc683c501b2dd0c4d332
chifamba <chifamba@gmail.com>
2025-04-11 17:04:38 +0000

    feat: update Person class to handle relationships

--- src/person.py
+++ src/person.py
@@ -1,3 +1,6 @@
+from src.relationship import Relationship
+
+
 class Person:
     def __init__(self, first_name, last_name, date_of_birth, place_of_birth, date_of_death=None, place_of_death=None):
         self.first_name = first_name
@@ -6,3 +9,7 @@ class Person:
         self.place_of_birth = place_of_birth
         self.date_of_death = date_of_death
         self.place_of_death = place_of_death
+        self.relationships = []
+
+    def add_relationship(self, relationship: Relationship):
+        self.relationships.append(relationship)
