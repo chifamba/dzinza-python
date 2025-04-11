4c723260113f00524ef2ab70f4f25fb8c1e59263
chifamba <chifamba@gmail.com>
2025-04-11 17:03:40 +0000

    feat: add Relationship model

---
+++ src/relationship.py
@@ -0,0 +1,5 @@
+class Relationship:
+    def __init__(self, person1, person2, relationship_type):
+        self.person1 = person1
+        self.person2 = person2
+        self.type = relationship_type
