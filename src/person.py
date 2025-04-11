aab844115919a981fe617ca630eb8378932849a0
chifamba <chifamba@gmail.com>
2025-04-11 16:25:32 +0000

    feat: add User and Person models

---
+++ src/person.py
@@ -0,0 +1,8 @@
+class Person:
+    def __init__(self, first_name, last_name, date_of_birth, place_of_birth, date_of_death=None, place_of_death=None):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.date_of_birth = date_of_birth
+        self.place_of_birth = place_of_birth
+        self.date_of_death = date_of_death
+        self.place_of_death = place_of_death
---
+++ src/user.py
@@ -0,0 +1,5 @@
+class User:
+    def __init__(self, user_id, email, password):
+        self.user_id = user_id
+        self.email = email
+        self.password = password
