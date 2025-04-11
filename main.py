2784c1c59147993b301801b42425d21b26a37e66
chifamba <chifamba@gmail.com>
2025-04-11 16:36:24 +0000

    feat: add creation user to main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from src.user_management import UserManager  # Import the UserManager class
-from src.user import User  # Import the User class
+from src.user_management import UserManager # Import the UserManager class
+from src.user import User # Import the User class
 
 def main():
-    # Create an instance of UserManager
+    # Create an instance of UserManager 
     user_manager = UserManager()
 
-    # Create a new user
-    user_id = 1  # User ID must be an integer
-    email = "test@example.com"  # Email must be a string
-    password = "password123"  # Password must be a string
+    # Create a new user with an id, email and password
+    user_id = 1 # User ID must be an integer
+    email = "test@example.com" # Email must be a string
+    password = "password123" # Password must be a string
     user_manager.create_user(user_id, email, password)
 
 if __name__ == '__main__':
