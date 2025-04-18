26767c4186f928695f20210b0d9e59ea0f6a31d8
chifamba <chifamba@gmail.com>
2025-04-18 19:40:18 +0000

    feat: replace cdn by bundled css

--- devserver.sh
+++ devserver.sh
@@ -6,11 +6,12 @@ export BACKEND_PORT=8090
 export FRONTEND_PORT=5173
 
 # Start backend
-cd backend
+cd backend 
 source .venv/bin/activate
 export FLASK_APP=app.py
 export FLASK_DEBUG=1
 flask run --port=$BACKEND_PORT &
+
 
 # Start frontend
 cd ..
