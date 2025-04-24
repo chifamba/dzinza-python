e1f1401c73e497d90a7ac4cc304f4bba6fc44100
chifamba <chifamba@gmail.com>
2025-04-24 03:01:11 +0000

    fix dev.nix config

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,10 +1,12 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://firebase.google.com/docs/studio/customize-workspace
-{ pkgs, ... }: {
+{ pkgs, ...}: {
   # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstable"
+  channel = "stable-24.11"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
+    pkgs.docker
+    pkgs.docker-compose  # Added Docker Compose
     pkgs.python3
     pkgs.python3Packages.transformers
     pkgs.python3Packages.numpy
@@ -15,23 +17,34 @@
     pkgs.python3Packages.accelerate
     pkgs.tree-sitter
     pkgs.black
-    pkgs.prettier
+    pkgs.vim
     pkgs.git
   ];
+  
+  services = {
+    docker = {
+      enable = true;
+    };
+  };
+
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-    extensions = [ "ms-python.python" ];
+    extensions = [ 
+      "ms-python.python" 
+      "ms-azuretools.vscode-docker"  # Added Docker/Docker Compose extension
+    ];
     workspace = {
       # Runs when a workspace is first created with this `dev.nix` file
       onCreate = {
         install = "";
         # Open editors for the following files by default, if they exist:
-        default.openFiles = [ "README.md" "src/index.html" "main.py" ];
+        default.openFiles = [ "README.md" "todo.md" ];
       }; # To run something each time the workspace is (re)started, use the `onStart` hook
     };
+
     # Enable previews and customize configuration
     previews = {
-      enable = true;
+      enable = false;
       previews = {
         web = {
           command = [ "./devserver.sh" ];
