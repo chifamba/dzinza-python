18a7941d39c3134c5891df8615b47fde653cce09
chifamba <chifamba@gmail.com>
2025-04-24 01:51:17 +0000

    minor edits

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -4,7 +4,20 @@
   # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
-  packages = [ pkgs.python3 ];
+  packages = [
+    pkgs.python3
+    pkgs.python3Packages.transformers
+    pkgs.python3Packages.numpy
+    pkgs.python3Packages.requests
+    pkgs.python3Packages.sentencepiece
+    pkgs.python3Packages.torch
+    pkgs.python3Packages.datasets
+    pkgs.python3Packages.accelerate
+    pkgs.tree-sitter
+    pkgs.black
+    pkgs.prettier
+    pkgs.git
+  ];
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [ "ms-python.python" ];
